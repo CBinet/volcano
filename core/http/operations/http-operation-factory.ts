3f5a9bcf98ee100f5824da8433fb3ae81cc8f943
Charles <cbinet1111@gmail.com>
2018-08-15 21:52:42 -0400

    publish 1.0.32

--- core/http/operations/http-operation-factory.ts
+++ core/http/operations/http-operation-factory.ts
@@ -23,6 +23,8 @@ export class HttpOperationFactory {
                     if (result) {
                         result.sendWith(response);
                     }
+                }).catch(error => {
+                    this.sendErrorDetails(error, response);
                 });
             }
         }
